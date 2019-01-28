dab917cdec7454bdc32527caca2809373c4f6e6c
jack austin <jackaustin@jacks-MacBook-Air.local>
2019-01-28 11:10:04 -0500

    final

--- app.py
+++ app.py
@@ -3,31 +3,34 @@ from redis import Redis, RedisError
 import os
 import socket
 
-# Connect to Redis
+# connect to redis
 redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 
 app = Flask(__name__)
 
+# home page
 @app.route("/")
+# basic hello world function
 def hello():
+    # try catch before opening then page so we don't shut down if DB is off
     try:
+        #increment the redis counter
         visits = redis.incr("counter")
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
+    #basic flask-html and redis access
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
 
+#create a new page /new to do some fun stuff
 @app.route("/new")
 def hellonew():
-    try:
-        visits = redis.incr("counter")
-    except RedisError:
-        visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    html = "<h3>Hello, New: {name}!</h3>" \
+    # testing the build
+    html = "<h3>Helloo, New: {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
